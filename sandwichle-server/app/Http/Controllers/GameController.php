8a6bbab14ac9fee0fe21e1bc72a5b0866f190915
Alex Miles <ghostaldev@gmail.com>
2022-03-10 17:55:50 +0000

    Remove unused import

--- sandwichle-server/app/Http/Controllers/GameController.php
+++ sandwichle-server/app/Http/Controllers/GameController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class GameController extends Controller
 {
